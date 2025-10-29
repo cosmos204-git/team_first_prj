cf5097350773a612c2dfa9baa599b15dbb9bc997
USER <USER@DESKTOP-0D2SNVJ>
2025-10-29 22:04:14 +0900

    학업관리 부분 수정

--- team_first_prj/src/kr/co/sist/stu/service/StuCourseMgrService.java
+++ team_first_prj/src/kr/co/sist/stu/service/StuCourseMgrService.java
@@ -1,15 +1,19 @@
 package kr.co.sist.stu.service;
 
 import java.util.List;
+
+import kr.co.sist.login.dao.CurrentStuData;
 import kr.co.sist.stu.dao.StuCourseMgrDAO;
 import kr.co.sist.stu.dto.StuCourseMgrDTO;
 
 public class StuCourseMgrService {
 
     public List<StuCourseMgrDTO> selectAllCourse() {
-        StuCourseMgrDAO stucmDTO = StuCourseMgrDAO.getInstance();
+        StuCourseMgrDAO dao = StuCourseMgrDAO.getInstance();
         try {
-            return stucmDTO.selectAllCourse();
+            int courseCode = CurrentStuData.getCourseCode();
+
+            return dao.selectAllCourse(courseCode);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
